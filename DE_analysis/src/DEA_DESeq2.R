d3f8a300654a3223dfe879e2482beac2791c4e2c
Hectorjlo <hectorjl@lcg.unam.mx>
2026-05-10 00:57:48 -0600

    Add implementation for pseudocounts and add a better CLI parser

--- DE_analysis/src/DEA_DESeq2.R
+++ DE_analysis/src/DEA_DESeq2.R
@@ -2,13 +2,15 @@
 ## Documentación
 
 ###
-# Uso: Rscript DEA_DESeq2.R <gene_counts_file> <annotacion_file> <gene_map_file> <result_dir> 
+# Uso: Rscript DEA_DESeq2.R <gene_counts_or_txi_file> <annotacion_file> <gene_map_file> <result_dir> 
 ###
 
 # Desactiva el dispositivo PDF
+## Evita generar archivos fuera de los indicados
 pdf(file = NULL)
 
 # Carga de las librerías necesarias para el análisis
+# Suprime Warnings para una salida stdout más limpia
 suppressWarnings(
     suppressPackageStartupMessages({
         library(DESeq2)
@@ -18,36 +20,90 @@ suppressWarnings(
         library(tibble)
         library(edgeR)
         library(circlize)
+        library(optparse)
     })
 )
 
-# Obten los argumentos del paseo de la CLI
-args <- commandArgs(trailingOnly = TRUE)
-gene_counts_path <- args[[1]]
-annotacion_file_path <- args[[2]]
-gene_name_map_file_path <- args[[3]]
-results_files_dir <- args[[4]]
+# Genera un parseador
+parser <- list(
+    # Obtiene el path de counts o txi
+    make_option(
+        c("--counts", "--txi"),
+        type = "character"
+    ),
+    # Obtiene el path del archivo de anotación
+    make_option(
+        "--annotation",
+        type = "character"
+    ),
+    # Obtiene el path del archivo gene_map (Geneid - gene_name) 
+    make_option(
+        "--gene_map",
+        type = "character"
+    ),
+    # Obtiene el path de la carpeta resultados
+    make_option(
+        "--results_dir",
+        type = "character"
+    ),
+    # Bandera para diferenciar si se procesara un objeto txi
+    # o una tabla de conteos
+    make_option(
+        "--from_pseudoalignment",
+        action = "store_true",
+        default = FALSE
+    )
+)
 
+# Parsea el objeto para ser accedido por índice
+agrs <- parse_args(OptionParser(option_list = parser))
+
+# Obten los argumentos del paseo de la CLI
+gene_counts_or_txi_path <- args$counts
+annotacion_file_path <- args$annotation
+gene_name_map_file_path <- args$gene_map
+results_files_dir <- args$results_dir
+from_pseudoalignment <- args$from_pseudoalignment
+
+## Para el path de "results" elimina si encuentra a un "/"
+## y agrega un "/", asegurando una única aparición 
 results_files_dir <- gsub("/*$", "/", results_files_dir)
 
-# Lee los archivos de conteos, anotación y del genemap
-gene_counts <- read.delim(gene_counts_path, row.names=1)
+# Lee los archivos de anotación y del genemap
 annotation <- read.delim(annotacion_file_path, row.names=1)
 gene_name_map <- read.delim(gene_name_map_file_path, header=FALSE, row.names=1)
 
-# Secuencia ordenada de cabeceras
+# Secuencia ordenada de cabeceras de los datos en procesamiento
+#!
 reordered <- c("male_24m_1", "male_24m_2", "male_24m_4", "male_24m_7",
                 "male_3m_3", "male_3m_5", "male_3m_6")
 
-# Reordena las columnas por edad
-gene_counts <- gene_counts[ ,reordered]
-
+# Si es un objeto txi
+if (from_pseudoalignment) {
+    # Lee el objeto txi
+    gene_counts_or_txi <- readRDS(gene_counts_or_txi_path)
+    # Registra el nombre de cada SRR respecto a la condición
+    #!
+    condition <- c("male_24m_2", "male_24m_1", "male_3m_5", "male_24m_7", "male_3m_6", "male_3m_3", "male_24m_4")
+    # Renombra los nombres de las columnas
+    colnames(gene_counts_or_txi$abundance) <- condition
+    colnames(gene_counts_or_txi$counts) <- condition
+    colnames(gene_counts_or_txi$length) <- condition
+    # Reordena las columnas por edad
+    gene_counts_or_txi$abundance <- gene_counts_or_txi$abundance[ ,reordered]
+    gene_counts_or_txi$counts <- gene_counts_or_txi$counts[ ,reordered]
+    gene_counts_or_txi$length <- gene_counts_or_txi$length[ ,reordered]
+} else { # Si es una tabla de conteos
+    # Lee el archivo de conteos
+    gene_counts_or_txi <- read.delim(gene_counts_or_txi_path, row.names=1)
+    # Reordena las columnas por edad
+    gene_counts_or_txi <- gene_counts_or_txi[ ,reordered]
+}
 
 # Generar la tabla de metadatos
-# Verifica con colnames(gene_counts), comprueba la cabecera de los archivos
+# Verifica con colnames(gene_counts_or_txi), comprueba la cabecera de los archivos
 ## Para estos archivos es:
 ## "male_24m_1" "male_24m_2" "male_24m_4" "male_24m_7" "male_3m_3"  "male_3m_5"  "male_3m_6"
-
 ## Por cual, primero crea un factor con el mismo orden que la cabecera en "age"
 age <- factor(c(rep("m24", 4), rep("m3", 3)),
     levels = c("m3", "m24"))
@@ -57,19 +113,25 @@ sex <- factor(c(rep("male", 7)))
 ## lightblue <- male_3m
 ## blue <- male_24m
 sample_color <- c(rep("blue", 4), rep("lightblue", 3))
-## Por último los colnames() 
-sample_names <- colnames(gene_counts)
+# Si es un objeto txi
+if (from_pseudoalignment) {
+    ## Por último los colnames() 
+    sample_names <- colnames(gene_counts_or_txi$counts)
+} else { # Si es una tabla de conteos
+    ## Por último los colnames() 
+    sample_names <- colnames(gene_counts_or_txi)
+}
 # Con los valores anteriores genera la tabla de metadatos
 meta_data <- data.frame(sample_names, age, sex)
 ## Genera
 ##   sample_names age  sex
-## 1   male_24m_2 m24 male
-## 2   male_24m_1 m24 male
-## 3    male_3m_5  m3 male
+## 1   male_24m_1 m24 male
+## 2   male_24m_2 m24 male
+## 3   male_24m_4 m24 male
 ## 4   male_24m_7 m24 male
-## 5    male_3m_6  m3 male
-## 6    male_3m_3  m3 male
-## 7   male_24m_4 m24 male
+## 5    male_3m_3  m3 male
+## 6    male_3m_5  m3 male
+## 7    male_3m_6  m3 male
 # Elimina la columna "sample_names", pero usa su contenido
 # como nombre de filas
 meta_data <- meta_data %>% 
@@ -77,19 +139,19 @@ meta_data <- meta_data %>%
         column_to_rownames(var="sample_names") 
 ## Genera
 ##            age  sex
-## male_24m_2 m24 male
 ## male_24m_1 m24 male
-## male_3m_5   m3 male
-## male_24m_7 m24 male
-## male_3m_6   m3 male
-## male_3m_3   m3 male
+## male_24m_2 m24 male
 ## male_24m_4 m24 male
+## male_24m_7 m24 male
+## male_3m_3   m3 male
+## male_3m_5   m3 male
+## male_3m_6   m3 male
 
-# Verifica que en los nombres de columnas en gene_counts
+# Verifica que en los nombres de columnas en gene_counts_or_txi
 # esten el mismo orden que los nombred de filas en meta_data
-print("Verificación: colnames(gene_counts) == rownames(meta_data)")
+print("Verificación: colnames(gene_counts_or_txi) == rownames(meta_data)")
 print(ifelse(
-    (all(colnames(gene_counts) == rownames(meta_data))),
+    (all(colnames(gene_counts_or_txi) == rownames(meta_data))),
     "Ok",
     "Error"
 ))
@@ -100,11 +162,24 @@ print(ifelse(
 ## lo cual permite poder crear los contrastes de comparación
 ## de manera más fina
 formula <- ~ 0 + age
-dds <- DESeqDataSetFromMatrix(
-    countData = round(gene_counts),
-    colData = meta_data,
-    design = formula
-)
+
+# Si es un objeto txi
+if (from_pseudoalignment) {
+    # Utiliza la formula para objetos de tximport
+    dds <- DESeqDataSetFromTximport(
+        txi = gene_counts_or_txi, 
+        colData = meta_data, 
+        design = formula
+    )
+} else { # Utiliza la formula para una tabla de conteos
+    dds <- DESeqDataSetFromMatrix(
+        countData = round(gene_counts_or_txi),
+        colData = meta_data,
+        design = formula
+    )
+}
+
+# Crea la matriz de diseño
 design <- model.matrix(formula)
 
 # Muestra genes totales
@@ -125,8 +200,17 @@ rm(keep)
 # Convierte a vst
 vsd <- vst(dds)
 # Genera el PCA plot
-plotPCA(vsd, intgroup = "age") +  
+PCA_plot <- plotPCA(vsd, intgroup = "age") +  
     theme_minimal(base_size = 18, base_line_size = 1)
+
+# Guarda el gráfico de PCA
+ggsave(filename = paste(results_files_dir, "plots/PCA_plot.png", sep = ""), 
+    plot = PCA_plot, 
+    dpi = 300, 
+    width = 11.25, 
+    height = 7.5,
+    create.dir = TRUE
+)
 
 # Calcula los factores de normalización,
 # varianza y ajusta el modelo
@@ -144,7 +228,6 @@ fpkm_to_tpm_log2 <- function(fpkm) {
 } 
 # Aplica la función por columnas (2)
 log2_tmp <- apply(log2_fpkm, 2, fpkm_to_tpm_log2)
-
 # Guarda en una tabla
 gene_names <- gene_name_map[rownames(log2_tmp), ]
 write.table(
@@ -202,7 +285,6 @@ write.table(
     quote = FALSE,
     row.names = T
 )
-
 write.table(
     res[down, ],
     paste(results_files_dir, "deseq-DEG-down", 
@@ -250,9 +332,7 @@ volcano_plot <- ggplot(
         base_size = 18,
         base_line_size = 1
     )
-
-
-
+# Guarda el gráfico de volcán
 ggsave(filename = paste(results_files_dir, "plots/vulcano_plot.png", sep = ""), 
     plot = volcano_plot, 
     dpi = 300, 
@@ -289,7 +369,7 @@ heatmap_plot <- Heatmap(
     col = colorRamp2(c(-2, -1, 0, 1, 2), 
         c("#07f900", "#007500", "#000000", "#750b00", "#ff2500"))
 )
-
+# Guarda el mapa de calor
 png(paste(results_files_dir, "plots/heatmap_all.png", sep = ""), 
     width = 11.25, height = 7.5, res = 300, units = "in")
 draw(heatmap_plot)
@@ -309,8 +389,7 @@ top_20_heatmap <- Heatmap(
     col = colorRamp2(c(-2, -1, 0, 1, 2), 
         c("#07f900", "#007500", "#000000", "#750b00", "#ff2500"))
 )
-
-
+# Guarda el mapa de calor
 png(paste(results_files_dir, "plots/heatmap_top20.png", sep = ""), 
     width = 11.25, height = 7.5, res = 300, units = "in")
 draw(top_20_heatmap)
--- DE_analysis/src/DEA_EdgeR.R
+++ DE_analysis/src/DEA_EdgeR.R
@@ -2,13 +2,15 @@
 ## Documentación
 
 ###
-# Uso: Rscript DEA_DESeq2.R <gene_counts_file> <annotacion_file> <gene_map_file> <result_dir> 
+# Uso: Rscript DEA_DESeq2.R <gene_counts_or_txi_file> <annotacion_file> <gene_map_file> <result_dir> 
 ###
 
 # Desactiva el dispositivo PDF
+## Evita generar archivos fuera de los indicados
 pdf(file = NULL)
 
 # Carga de las librerías necesarias para el análisis
+# Suprime Warnings para una salida stdout más limpia
 suppressWarnings(
     suppressPackageStartupMessages({
         library(ggplot2)
@@ -17,36 +19,90 @@ suppressWarnings(
         library(tibble)
         library(edgeR)
         library(circlize)
+        library(optparse)
     })
 )
 
-# Obten los argumentos del paseo de la CLI
-args <- commandArgs(trailingOnly = TRUE)
-gene_counts_path <- args[[1]]
-annotacion_file_path <- args[[2]]
-gene_name_map_file_path <- args[[3]]
-results_files_dir <- args[[4]]
+# Genera un parseador
+parser <- list(
+    # Obtiene el path de counts o txi
+    make_option(
+        c("--counts", "--txi"),
+        type = "character"
+    ),
+    # Obtiene el path del archivo de anotación
+    make_option(
+        "--annotation",
+        type = "character"
+    ),
+    # Obtiene el path del archivo gene_map (Geneid - gene_name) 
+    make_option(
+        "--gene_map",
+        type = "character"
+    ),
+    # Obtiene el path de la carpeta resultados
+    make_option(
+        "--results_dir",
+        type = "character"
+    ),
+    # Bandera para diferenciar si se procesara un objeto txi
+    # o una tabla de conteos
+    make_option(
+        "--from_pseudoalignment",
+        action = "store_true",
+        default = FALSE
+    )
+)
 
+# Parsea el objeto para ser accedido por índice
+agrs <- parse_args(OptionParser(option_list = parser))
+
+# Obten los argumentos del paseo de la CLI
+gene_counts_or_txi_path <- args$counts
+annotacion_file_path <- args$annotation
+gene_name_map_file_path <- args$gene_map
+results_files_dir <- args$results_dir
+from_pseudoalignment <- args$from_pseudoalignment
+
+## Para el path de "results" elimina si encuentra a un "/"
+## y agrega un "/", asegurando una única aparición 
 results_files_dir <- gsub("/*$", "/", results_files_dir)
 
-# Lee los archivos de conteos, anotación y del genemap
-gene_counts <- read.delim(gene_counts_path, row.names=1)
+# Lee los archivos de anotación y del genemap
 annotation <- read.delim(annotacion_file_path, row.names=1)
 gene_name_map <- read.delim(gene_name_map_file_path, header=FALSE, row.names=1)
 
-# Secuencia ordenada de cabeceras
+# Secuencia ordenada de cabeceras de los datos en procesamiento
+#!
 reordered <- c("male_24m_1", "male_24m_2", "male_24m_4", "male_24m_7",
-                "male_3m_3", "male_3m_5", "male_3m_6")
+"male_3m_3", "male_3m_5", "male_3m_6")
 
-# Reordena las columnas por edad
-gene_counts <- gene_counts[ ,reordered]
-
+# Si es un objeto txi
+if (from_pseudoalignment) {
+    # Lee el objeto txi
+    gene_counts_or_txi <- readRDS(gene_counts_or_txi_path)
+    # Registra el nombre de cada SRR respecto a la condición
+    #!
+    condition <- c("male_24m_2", "male_24m_1", "male_3m_5", "male_24m_7", "male_3m_6", "male_3m_3", "male_24m_4")
+    # Renombra los nombres de las columnas  
+    colnames(gene_counts_or_txi$abundance) <- condition
+    colnames(gene_counts_or_txi$counts) <- condition
+    colnames(gene_counts_or_txi$length) <- condition
+    # Reordena las columnas por edad
+    gene_counts_or_txi$abundance <- gene_counts_or_txi$abundance[ ,reordered]
+    gene_counts_or_txi$counts <- gene_counts_or_txi$counts[ ,reordered]
+    gene_counts_or_txi$length <- gene_counts_or_txi$length[ ,reordered]
+} else { # Si es una tabla de conteos
+    # Lee el archivo de conteos
+    gene_counts_or_txi <- read.delim(gene_counts_or_txi_path, row.names=1)
+    # Reordena las columnas por edad
+    gene_counts_or_txi <- gene_counts_or_txi[ ,reordered]
+}
 
 # Generar la tabla de metadatos
-# Verifica con colnames(gene_counts), comprueba la cabecera de los archivos
+# Verifica con colnames(gene_counts_or_txi), comprueba la cabecera de los archivos
 ## Para estos archivos es:
 ## "male_24m_1" "male_24m_2" "male_24m_4" "male_24m_7" "male_3m_3"  "male_3m_5"  "male_3m_6"
-
 ## Por cual, primero crea un factor con el mismo orden que la cabecera en "age"
 age <- factor(c(rep("m24", 4), rep("m3", 3)),
     levels = c("m3", "m24"))
@@ -56,19 +112,25 @@ sex <- factor(c(rep("male", 7)))
 ## lightblue <- male_3m
 ## blue <- male_24m
 sample_color <- c(rep("blue", 4), rep("lightblue", 3))
-## Por último los colnames() 
-sample_names <- colnames(gene_counts)
+# Si es un objeto txi
+if (from_pseudoalignment) {
+    ## Por último los colnames() 
+    sample_names <- colnames(gene_counts_or_txi$counts)
+} else { # Si es una tabla de conteos
+    ## Por último los colnames() 
+    sample_names <- colnames(gene_counts_or_txi)
+}
 # Con los valores anteriores genera la tabla de metadatos
 meta_data <- data.frame(sample_names, age, sex)
 ## Genera
 ##   sample_names age  sex
-## 1   male_24m_2 m24 male
-## 2   male_24m_1 m24 male
-## 3    male_3m_5  m3 male
+## 1   male_24m_1 m24 male
+## 2   male_24m_2 m24 male
+## 3   male_24m_4 m24 male
 ## 4   male_24m_7 m24 male
-## 5    male_3m_6  m3 male
-## 6    male_3m_3  m3 male
-## 7   male_24m_4 m24 male
+## 5    male_3m_3  m3 male
+## 6    male_3m_5  m3 male
+## 7    male_3m_6  m3 male
 # Elimina la columna "sample_names", pero usa su contenido
 # como nombre de filas
 meta_data <- meta_data %>% 
@@ -76,19 +138,19 @@ meta_data <- meta_data %>%
         column_to_rownames(var="sample_names") 
 ## Genera
 ##            age  sex
-## male_24m_2 m24 male
 ## male_24m_1 m24 male
-## male_3m_5   m3 male
-## male_24m_7 m24 male
-## male_3m_6   m3 male
-## male_3m_3   m3 male
+## male_24m_2 m24 male
 ## male_24m_4 m24 male
+## male_24m_7 m24 male
+## male_3m_3   m3 male
+## male_3m_5   m3 male
+## male_3m_6   m3 male
 
-# Verifica que en los nombres de columnas en gene_counts
+# Verifica que en los nombres de columnas en gene_counts_or_txi
 # esten el mismo orden que los nombred de filas en meta_data
-print("Verificación: colnames(gene_counts) == rownames(meta_data)")
+print("Verificación: colnames(gene_counts_or_txi) == rownames(meta_data)")
 print(ifelse(
-    (all(colnames(gene_counts) == rownames(meta_data))),
+    (all(colnames(gene_counts_or_txi) == rownames(meta_data))),
     "Ok",
     "Error"
 ))
@@ -99,8 +161,31 @@ print(ifelse(
 ## lo cual permite poder crear los contrastes de comparación
 ## de manera más fina
 formula <- ~ 0 + age
+
+# Si es un objeto txi
+if (from_pseudoalignment) {
+    # Crea un objeto DGE utilizando los conteos del objeto txi
+    dge <- DGEList(counts = gene_counts_or_txi$counts)
+    # Corrige los conteos dada la longitud
+    # Crea un factor de normalización
+    normalized_factor <- gene_counts_or_txi$length / exp(
+        rowMeans(log(gene_counts_or_txi$length))
+    )
+    # Normaliza los conteos dado el factor de normalización
+    normalized_counts <- gene_counts_or_txi$counts / normalized_factor
+    # Calcula el tamaño efectivo de la libreria
+    effective_library_size <- calcNormFactors(normalized_counts) * colSums(normalized_counts)
+    # Aplica el factor de normalización a la libreria
+    normalized_factor <- sweep(normalized_factor, 2, effective_library_size, "*")
+    
+    # Agrega los conteos normalizados aplicado en log
+    dge$offset <- log(normalized_factor)
+} else { # Si es una tabla de conteos
+    dge <- DGEList(counts = round(gene_counts_or_txi))
+}
+
+# Crea la matriz de diseño
 design <- model.matrix(formula)
-dge <- DGEList(counts = round(gene_counts))
 
 # Muestra genes totales
 print("Genes totales:")
@@ -115,8 +200,13 @@ dge <- dge[keep, , keep.lib.sizes = FALSE]
 # Elimina el filtro por uso de memoria
 rm(keep)
 
+# Si es una tabla de conteos
+if (!(from_pseudoalignment)) {
+    # Normaliza los conteos
+    dge <- calcNormFactors(dge, method = "TMM")
+}
+
 ## Se creará el PCA plot usando logCPM
-dge <- calcNormFactors(dge, method = "TMM")
 log_cpm <- cpm(dge, log = TRUE, prior.count = 1)
 pca_res <- prcomp(t(log_cpm), scale. = TRUE)
 pca_df <- data.frame(
@@ -124,16 +214,26 @@ pca_df <- data.frame(
     PC2 = pca_res$x[, 2],
     age = meta_data$age
 )
-ggplot(pca_df, aes(x = PC1, y = PC2, color = age)) +
+# Genera el PCA plot
+PCA_plot <- ggplot(pca_df, aes(x = PC1, y = PC2, color = age)) +
     geom_point(size = 3) +
     theme_minimal(base_size = 18, base_line_size = 1)
+# Guarda el gráfico de PCA
+ggsave(filename = paste(results_files_dir, "plots/PCA_plot.png", sep = ""), 
+    plot = PCA_plot, 
+    dpi = 300, 
+    width = 11.25, 
+    height = 7.5,
+    create.dir = TRUE
+)
 
 # Estima la dispersión y ajusta el modelo
 dge <- estimateDisp(dge, design, robust = TRUE)
+# Utiliza glmFit para ser comparable a DESeq2
 fit <- glmFit(dge, design, robust = TRUE)
 
 ## Se calculó los TPM, métrica importante pero no indespensable
-# Añade la longitud de los genes (en bp)
+# Añade la longitud de los genes 
 gene_lengths <- annotation[rownames(dge), ]
 # Calcula el log_2 RPKM
 ## Se añadió un pseudo conteo para evitar log2(0)
@@ -144,7 +244,6 @@ fpkm_to_tpm_log2 <- function(fpkm) {
 } 
 # Aplica la función por columnas (2)
 log2_tmp <- apply(log2_rpkm, 2, fpkm_to_tpm_log2)
-
 # Guarda en una tabla
 gene_names <- gene_name_map[rownames(log2_tmp), ]
 write.table(
@@ -170,7 +269,10 @@ contrasts <- makeContrasts(m24_vs_m3 = agem24 - agem3,
 # Realiza el análisis de expresión diferencial
 ## En este caso al ser solo uno es directo
 ## Se usa el contraste comparando agem24 vs agem3
+# Utiliza glmLRT para ser comparable con DESeq2
 glm_LRT <- glmLRT(fit, contrast = contrasts[, "m24_vs_m3"])
+# Obten los genes más expresados diferencialmente
+## Evitando el castigo por muchos genes en el FDR
 res <- topTags(glm_LRT, n = Inf, sort.by = "none")$table
 # Añade Gene name
 res$Gene_name <- gene_name_map[rownames(res), ]
@@ -203,7 +305,6 @@ write.table(
     quote = FALSE,
     row.names = T
 )
-
 write.table(
     res[down, ],
     paste(results_files_dir, "deseq-DEG-down", 
@@ -251,9 +352,7 @@ volcano_plot <- ggplot(
         base_size = 18,
         base_line_size = 1
     )
-
-
-
+# Guarda el gráfico de volcán
 ggsave(filename = paste(results_files_dir, "plots/vulcano_plot.png", sep = ""), 
     plot = volcano_plot, 
     dpi = 300, 
@@ -290,7 +389,7 @@ heatmap_plot <- Heatmap(
     col = colorRamp2(c(-2, -1, 0, 1, 2), 
         c("#07f900", "#007500", "#000000", "#750b00", "#ff2500"))
 )
-
+# Guarda el mapa de calor
 png(paste(results_files_dir, "plots/heatmap_all.png", sep = ""), 
     width = 11.25, height = 7.5, res = 300, units = "in")
 draw(heatmap_plot)
@@ -310,8 +409,7 @@ top_20_heatmap <- Heatmap(
     col = colorRamp2(c(-2, -1, 0, 1, 2), 
         c("#07f900", "#007500", "#000000", "#750b00", "#ff2500"))
 )
-
-
+# Guarda el mapa de calor
 png(paste(results_files_dir, "plots/heatmap_top20.png", sep = ""), 
     width = 11.25, height = 7.5, res = 300, units = "in")
 draw(top_20_heatmap)
