65315225a88670557d47f534781a9a7c9f36b0df
Hectorjlo <hectorjl@lcg.unam.mx>
2026-05-20 09:50:42 -0600

    Add comments to file

--- DE_analysis/src/GSEA.R
+++ DE_analysis/src/GSEA.R
@@ -1,39 +1,55 @@
+# Carga librerías necesarias
 library(DESeq2)
 library(org.Mm.eg.db)
 library(clusterProfiler)
 library(enrichplot)
 library(ggplot2)
 
-
+# Carga el objeto RDS generado del análisis diferencial
 res <- readRDS(file = "DE_analysis/results/star/DE_analysis/DESeq2/paired_end/results.rds")
 
+# Ordena de manera inversa por stat
 res <- res[order(-res$stat), ]
 
+# Crea la lista de genes
 gene_list <- res$stat
-names(gene_list) <- rownames(res)
+# Renombra el vector igual que los nombres de los genes del objeto RDS
+names(gene_list) <- rownames(res) 
 
+# Realiza el análisis de GSE
 gse <- gseGO(geneList = gene_list,
-            ont = "BP",
-            keyType = "ENSEMBL",
-            OrgDb = "org.Mm.eg.db",
-            eps = 1e-300
+            ont = "BP", # Agrega la ontología "Biological Process"
+            keyType = "ENSEMBL", # Formato ENSEMBL ID
+            OrgDb = "org.Mm.eg.db", # Base de datos de ratón
+            eps = 1e-300 # No trunca el P-value hasta 1e-300
         )
 
+# Guarda el objeto RDS generado
 saveRDS(gse, file = "DE_analysis/results/star/DE_analysis/DESeq2/paired_end/gse.rds")
 
+# Realiza el plot
 p <- gseaplot2(gse,
-               geneSetID = c(grep("osteoclast proliferation", as.data.frame(gse)$Description), 
-                            grep("negative regulation of osteoclast differentiation", as.data.frame(gse)$Description)),
+               geneSetID = c(grep(
+                "osteoclast proliferation", 
+                as.data.frame(gse)$Description
+                ), # Busca a ese patrón retorna el índice
+                grep("negative regulation of osteoclast differentiation", 
+                as.data.frame(gse)$Description)
+                ),
                title = "",
                base_size = 14,
-               pvalue_table = TRUE,
-               pvalue_table_columns = c("NES", "p.adjust")
+               pvalue_table = TRUE, # Muestra la tabla de P-value
+               pvalue_table_columns = c("NES", "p.adjust") # Agrega el valor NES
             )
 
-p[[1]] <- p[[1]] +  geom_hline(yintercept = 0, linetype = "solid", color = "grey40", linewidth = 0.5)
+p[[1]] <- p[[1]] +  geom_hline( # Agrega una línea en y = 0
+    yintercept = 0, 
+    linetype = "solid", 
+    color = "grey40", 
+    linewidth = 0.5
+)
 
-p
-
+# Guarda el plot
 ggsave(filename = "DE_analysis/results/star/DE_analysis/DESeq2/paired_end/plots/GSEA_plot.png",
     plot = p, 
     dpi = 1200, 
